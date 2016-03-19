a55315bb5eb5f53472712d8f0e1c6598f1e0f1ca
Harry Chen <i@harrychen.xyz>
2016-03-19 20:29:10 +0800

    Update encoding switch tool
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- WinEdt Encoding Switcher/main.cpp
+++ WinEdt Encoding Switcher/main.cpp
@@ -4,29 +4,37 @@
 
 #include <tchar.h>
 #include <Windows.h>
-#include <ShlObj.h>
 
 using namespace std;
 
-#define ORIG_NAME _T("WinEdt.dnt")
-#define ANSI_NAME _T("ANSI.dnt")
-#define UTF8_NAME _T("UTF8.dnt")
-
 int main(int argc, wchar_t **argv) {
 
-	wchar_t AppDataPath[MAX_PATH];
-	SHGetFolderPath(NULL, CSIDL_APPDATA, NULL, NULL, AppDataPath);
-	wcscat_s(AppDataPath, _T("\\WinEdt Team\\WinEdt 10\\"));
+	wchar_t AppPath[MAX_PATH],
+		Utf8Path[MAX_PATH],
+		AnsiPath[MAX_PATH],
+		OrigPath[MAX_PATH];
+
+	GetModuleFileName(NULL, AppPath, sizeof(AppPath));
+	*wcsrchr(AppPath, '\\') = 0;
+	wcscat_s(AppPath, _T("\\"));
+
+	wcscpy_s(Utf8Path, AppPath);
+	wcscat_s(Utf8Path, _T("WinEdt.dnt.UTF8"));
+	wcscpy_s(AnsiPath, AppPath);
+	wcscat_s(AnsiPath, _T("WinEdt.dnt.ANSI"));
+	wcscpy_s(OrigPath, AppPath);
+	wcscat_s(OrigPath, _T("WinEdt.dnt"));
 
 
 	cout << "WinEdt 默认编码配置切换工具" << endl;
 	cout << "此工具随 CTeX 套装发布" << endl;
-	cout << "警告：如果使用此工具，则会丢失所有对 WinEdt 的自定义配置" << endl;
-	cout << "如果需要，可以备份以下目录内的文件：";
-	wcout << AppDataPath << endl << endl;
+	cout << "警告：如果使用此工具，则可能丢失所有对 WinEdt 的自定义配置" << endl;
+	cout << "如果需要，可以备份以下文件：";
+	wcout << OrigPath << endl;
+	cout << "在进行切换前，请先退出 WinEdt" << endl << endl;
 
 	
-	if (!(fstream(UTF8_NAME, ios::in) && (fstream(ANSI_NAME, ios::in)))) {
+	if (!(fstream(Utf8Path, ios::in) && (fstream(AnsiPath, ios::in)))) {
 		cerr << "配置文件不完整，可能是工作目录不正确或者已经损坏。" << endl;
 		cerr << "程序将退出，建议重新安装 CTeX 套装。" << endl << endl;
 		system("pause");
@@ -39,36 +47,21 @@ int main(int argc, wchar_t **argv) {
 	cout << "请输入编号：";
 	string selection;
 	cin >> selection;
+
 	BOOL success;
 	if (selection == "1") {
-		success = CopyFile(UTF8_NAME, ORIG_NAME, false);
+		success = CopyFile(Utf8Path, OrigPath, false);
 	}
 	else if (selection == "2") {
-		success = CopyFile(ANSI_NAME, ORIG_NAME, false);
+		success = CopyFile(AnsiPath, OrigPath, false);
 	}
 	else {
 		cerr << "输入不正确，请重新运行本程序。" << endl << endl;
 		system("pause");
 		return 2;
 	}
+
 	if (success) {
-		wchar_t FilePath[MAX_PATH];
-		WIN32_FIND_DATA FindFileData;
-		HANDLE hListFile;
-
-		wcscpy_s(FilePath, AppDataPath);
-		wcscat_s(FilePath, _T("*.*"));
-		hListFile = FindFirstFile(FilePath, &FindFileData);
-		if (hListFile != INVALID_HANDLE_VALUE)
-		{
-			do {
-				wchar_t FullPath[MAX_PATH];
-				wcscpy_s(FullPath, AppDataPath);
-				wcscat_s(FullPath, FindFileData.cFileName);
-				DeleteFile(FullPath);
-			} while (FindNextFile(hListFile, &FindFileData));
-		}
-
 		cout << endl << "编码切换成功，请按任意键退出。" << endl;
 		system("pause");
 	}
